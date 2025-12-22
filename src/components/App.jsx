aef9b10321dcdb1ec56c34eda83f97a25162da1c
PatriciaHiguera <patyhiguera@hotmail.com>
2025-12-21 23:46:45 -0600

    Cargando loader diseño y animación

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from '../assets/images/react.svg';
+import viteLogo from '/vite.svg';
+import '../assets/blocks/App.css';
+import './Loader.jsx';
+import Loader from './Loader.jsx';
+
+function App() {
+  return (
+    <>
+      <Loader></Loader>
+    </>
+  )
+}
+
+export default App;
