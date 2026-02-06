100ecc25ae3bd58bf0001350459877f0b949ac5a
PatriciaHiguera <patyhiguera@hotmail.com>
2026-02-05 23:43:06 -0600

    Fix deployment paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/flight-status-frontend',
   server: {
     port: 3000,
     open: true,
